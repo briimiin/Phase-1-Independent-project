dfa9493ea9db185a6982b79fbabb395f76a15562
briimiin <kipkorirbrimin005@gmail.com>
2024-04-12 15:45:11 +0300

    selects apod-image

--- nasa.js
+++ nasa.js
@@ -1,2 +1,7 @@
+// declare apikey and apod url
 const apiKey = "85nxQw84l2fm2nxzmKmEkeG2z2NtaaDBBL6zcWHt";
-const apodUrl = "https://api.nasa.gov/planetary/apod"
+const apodUrl = "https://api.nasa.gov/planetary/apod";
+
+function contentLoaded(){
+    const apodElement = document.querySelector(".apod-image img");
+}
